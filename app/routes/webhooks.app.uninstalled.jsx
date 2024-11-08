2e46cd442ad53330fdb6b17eab2f7ce45b909f3c
Neekha <neekha@decagrowth.com>
2024-11-08 10:22:14 +0530

    Signed-off-by: Neekha <neekha@decagrowth.com>

--- app/routes/webhooks.app.uninstalled.jsx
+++ app/routes/webhooks.app.uninstalled.jsx
@@ -4,7 +4,9 @@ import {deleteMetafieldDefinition  } from "../utils/shopify";
 
 export const action = async ({ request }) => {
   const { shop, session, topic } = await authenticate.webhook(request);
+  console.log(session)
   const access_token=session.accessToken
+  console.log(access_token)
   console.log(`Received ${topic} webhook for ${shop}`);
   const deleteResponse = await deleteMetafieldDefinition(access_token, shop);
 
@@ -12,7 +14,6 @@ export const action = async ({ request }) => {
   // Webhook requests can trigger multiple times and after an app has already been uninstalled.
   // If this webhook already ran, the session may have been deleted previously.
   if (session) {
-    await db.session.deleteMany({ where: { shop } });
     const response = await fetch(`https://reorderappapi.onrender.com/auth/markAppAsUnInstalled`, {
       method: "PATCH",
       headers: {
@@ -24,6 +25,8 @@ export const action = async ({ request }) => {
     if (!response.ok) {
       throw new Error("Failed to mark the app as installed");
     }
+    await db.session.deleteMany({ where: { shop } });
+    
   }
 
   return new Response();
