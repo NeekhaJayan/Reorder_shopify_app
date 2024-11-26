9b47b563ce0531f74c80319f28fe608eafe9d576
Neekha <neekha@decagrowth.com>
2024-11-27 00:52:27 +0530

    Signed-off-by: Neekha <neekha@decagrowth.com>

--- app/routes/webhooks.app.uninstalled.jsx
+++ app/routes/webhooks.app.uninstalled.jsx
@@ -2,33 +2,28 @@ import { authenticate } from "../shopify.server";
 import db from "../db.server";
 import {deleteMetafieldForAllProducts,deleteMetafieldDefinition  } from "../utils/shopify";
 
+
 export const action = async ({ request }) => {
   const { shop, topic } = await authenticate.webhook(request);
   const {admin,session }=await authenticate.admin(request);
-  console.log(session)
-  const access_token=session.accessToken
-  console.log(access_token)
   console.log(`Received ${topic} webhook for ${shop}`);
-  const deleteResponse = await deleteMetafieldForAllProducts(admin);
   
-  const deleteDefinition=await deleteMetafieldDefinition(admin);
-  console.log(`Deleted ${deleteResponse} from ${shop}`);
+  
   // Webhook requests can trigger multiple times and after an app has already been uninstalled.
   // If this webhook already ran, the session may have been deleted previously.
   if (session) {
-    const response = await fetch(`https://reorderappapi.onrender.com/auth//markAppAsUnInstalled`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ shop})
+    const session = await db.session.findUnique({
+      where: { shop }, // Find the session by shop field
+      select: { accessToken: true }, // Retrieve only the accessToken field
     });
-    
-    if (!response.ok) {
-      throw new Error("Failed to mark the app as installed");
+    if (!session || !session.accessToken) {
+      throw new Error(`Access token not found for shop: ${shop}`);
     }
+    const deleteResponse = await deleteMetafieldForAllProducts(session.accessToken,shop);
+  
+    const deleteDefinition=await deleteMetafieldDefinition(session.accessToken,shop);
+    console.log(`Deleted ${deleteResponse} from ${shop}`);
     await db.session.deleteMany({ where: { shop } });
-    
   }
 
   return new Response();
