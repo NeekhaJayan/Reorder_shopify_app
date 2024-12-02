0b88be751d7330aadcb51d69d2cbac0c2d7679f6
Neekha <neekha@decagrowth.com>
2024-12-02 13:54:25 +0530

    Signed-off-by: Neekha <neekha@decagrowth.com>

--- app/routes/webhooks.order.jsx
+++ app/routes/webhooks.order.jsx
@@ -5,30 +5,31 @@ export const action = async ({ request }) => {
   
 
   console.log(`Received ${topic} webhook for ${shop}:Payload is:${payload}`);
-  // console.log(request);
-  const product_ids = payload.line_items.map(item => ({
-    product_id: item.product_id,
-    quantity: item.quantity,
-  }));
-  const order_payload_details = {
-    order_id: payload.id,
-    customer_id: payload.customer.id,
-    customer_email: payload.customer.email,
-    customer_name: payload.customer.first_name,
-    customer_phone: payload.customer.phone,
-    line_items: payload.line_items.map(item => ({
-      product_id: item.product_id,
-      quantity: item.quantity,
-      price: item.price
-    })), 
-    order_date: payload.created_at,
-  };
+  console.log(payload);
+  // const product_ids = payload.line_items.map(item => ({
+  //   product_id: item.product_id,
+  //   quantity: item.quantity,
+  // }));
+  // const order_payload_details = {
+  //   order_id: payload.id,
+  //   customer_id: payload.customer.id,
+  //   customer_email: payload.customer.email,
+  //   customer_name: payload.customer.first_name,
+  //   customer_phone: payload.customer.phone,
+  //   line_items: payload.line_items.map(item => ({
+  //     product_id: item.product_id,
+  //     quantity: item.quantity,
+  //     price: item.price
+  //   })), 
+  //   order_date: payload.created_at,
+  // };
 
-  console.log(order_payload_details);
+  // console.log(order_payload_details);
 
-  return new Response(JSON.stringify(order_payload_details), {
-    headers: { "Content-Type": "application/json" },
-  });
+  // return new Response(JSON.stringify(order_payload_details), {
+  //   headers: { "Content-Type": "application/json" },
+  // });
+  return new Response();
 };
 
 
