9416df5582731ac996d24048305a300d5e229f5d
Neekha <neekha@decagrowth.com>
2025-01-14 09:50:32 +0530

    Signed-off-by: Neekha <neekha@decagrowth.com>

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -2,7 +2,7 @@ import "@shopify/shopify-app-remix/adapters/node";
 import {
   ApiVersion,
   AppDistribution,
-  shopifyApp,
+  shopifyApp,BillingInterval,
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import { restResources } from "@shopify/shopify-api/rest/admin/2024-10";
