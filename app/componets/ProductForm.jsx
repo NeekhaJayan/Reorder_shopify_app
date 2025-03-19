9b9820df93de298805453db9051b01441ded957f
neekha <neekha@decagrowth.com>
2025-03-19 17:26:02 +0530

    Signed-off-by: neekha <neekha@decagrowth.com>

--- app/componets/ProductForm.jsx
+++ app/componets/ProductForm.jsx
@@ -83,7 +83,7 @@ const ProductForm = ({ bannerMessage,bannerStatus,setBannerMessage,handleChange,
                             <TextField label="Estimated Usage Days " type="number" name="date" value={formState.date} onChange={handleChange} onBlur={handleBlur} autoComplete="off" />                    
                         </div>
                         <div style={{ display: 'grid', justifyContent: 'center' }}>
-                            {plan === "FREE" && updatedProducts.length >= FREE_PRODUCT_LIMIT ? (
+                            {plan === "FREE" && updatedProducts.length >= APP_SETTINGS.FREE_PRODUCT_LIMIT ? (
                             <Button variant="secondary" onClick={() => navigate("/app/settings?tab=2")}>
                                 Upgrade
                             </Button>
