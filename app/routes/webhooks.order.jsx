08929603f8d56251e2f96a60c63b407505a31f17
neekha <neekha@decagrowth.com>
2025-04-01 13:13:14 +0530

    Signed-off-by: neekha <neekha@decagrowth.com>

--- app/routes/webhooks.order.jsx
+++ app/routes/webhooks.order.jsx
@@ -1,4 +1,4 @@
-import shopify,{unauthenticated,authenticate} from "../shopify.server";
+import {authenticate} from "../shopify.server";
 
 export const action = async ({ request }) => {
   const { shop, payload, topic } = await authenticate.webhook(request);
