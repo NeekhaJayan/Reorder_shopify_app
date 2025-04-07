a987e5cdf4066674170e65dff394bda594a6d522
neekha <neekha@decagrowth.com>
2025-04-07 11:22:34 +0530

    Signed-off-by: neekha <neekha@decagrowth.com>

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -108,10 +108,13 @@ export default function SettingsPage() {
             }
           `}
         </style>
-        {showBanner && (
+        {showBanner && message && (
           <Banner tone="critical" onDismiss={() => setShowBanner(false)}>
-
-            {message}
+            <ul style={{ paddingLeft: '1.2rem', margin: 0 }}>
+              {message.map((msg, i) => (
+                <li key={i} style={{ marginBottom: '0.5rem' }}>{msg}</li>
+              ))}
+            </ul>
           </Banner>
         )}
         {emailSettingsbanner  && (
