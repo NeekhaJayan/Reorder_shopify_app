96f413c3caea2bbecaf4d15312a3b477d8957d76
neekha <neekha@decagrowth.com>
2025-04-28 18:49:43 +0530

    Signed-off-by: neekha <neekha@decagrowth.com>

--- app/componets/settings/PricingPlans.jsx
+++ app/componets/settings/PricingPlans.jsx
@@ -75,18 +75,19 @@ const PricingPlans = ({ plan } ) => {
                   
                   {plan.name === activePlan ? 'Current Plan' : 'Choose Plan'}
                 </Button>
+                {plan.name === 'Pro Plan' && (
+                  <div style={{ marginTop: '8px', textAlign: 'center' }}>
+                    <Text variant="bodySm" as="p" fontWeight="medium" tone="subdued">
+                      7-day free trial!
+                    </Text>
+                  </div>
+                )}
               </div>
-
+              
+    
             ))}
           </div>
-          {activePlan === 'Pro Plan' && (
-            <div style={{ marginTop: '20px', textAlign: 'center' }}>
-              <Text variant="bodyMd" as="p" fontWeight="medium" tone="subdued">
-                7-day free trial!
-              </Text>
-            </div>
-          )}
-
+          
         </div>
       </Card>
       <Modal
