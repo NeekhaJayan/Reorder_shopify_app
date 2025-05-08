847f4507f16c00de0c37cdd18c91a517078b4406
neekha <neekha@decagrowth.com>
2025-05-08 10:31:58 +0530

    Signed-off-by: neekha <neekha@decagrowth.com>

--- app/componets/settings/PricingPlans.jsx
+++ app/componets/settings/PricingPlans.jsx
@@ -71,7 +71,10 @@ const PricingPlans = ({ plan } ) => {
                     primary={plan.name === activePlan} 
                     outline={plan.name !== activePlan}
                     disabled={plan.name === activePlan}
-                    onClick={() => handleChoosePlan(plan.name)}
+                    // onClick={() => handleChoosePlan(plan.name)}
+                    target="_top"
+                    url="https://admin.shopify.com/charges/reorder-reminder-pro/pricing_plans"
+
                   >
                     
                     {plan.name === activePlan ? 'Current Plan' : 'Choose Plan'}
